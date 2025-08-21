04a0c2b141e667b489593a1f2230f467fb8dc010
MeetBhatiya23 <meetbhatiya.dds@gmail.com>
2025-08-21 16:04:53 +0530

    fixed-corss

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT =process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://shippin-go-client.vercel.app/",
+    origin: "https://shoppin-go.vercel.app",
   })
 );
 // MongoDB connection
