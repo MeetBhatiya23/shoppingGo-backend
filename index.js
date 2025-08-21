8ad67c0eb4dbe2fba4668b8df9f481bbfca8fe6f
MeetBhatiya23 <meetbhatiya.dds@gmail.com>
2025-08-21 14:42:02 +0530

    mongo-issues

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ app.use(
 );
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB connection error: ", err));
 
